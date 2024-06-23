a7b17fc6c4128ec8d30cf95128b01bae89f7a23f
Ehsan Eslami <eslami.ehsan200@gmail.com>
2024-06-23 14:00:51 +0330

    feat: add frontend mentor footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,10 @@ export default function Home() {
     <main>
       <Calc />
     </main>
+    <footer className="attribution fixed bottom-0 z-40 left-[50%] translate-x-[-50%]  py-1 px-2 rounded-3xl">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="https://github.com/Ehsan-eslami">Ehsan Eslami</a>.
+    </footer>
   </div>
   );
 }
